059756fe6e1d655520880ce3e23a4217bdac7499
Alex Kazik <alex@kazik.de>
2026-04-07 19:21:19 +0200

    Add Usage::check, improve Usage::add

--- src/usage.rs
+++ src/usage.rs
@@ -23,7 +23,7 @@ impl Usage {
     /// * If `character_count` is None, false is returned.
     /// * `character_count` is increased by `additional`
     /// * If `character_limit` is None, false is returned.
-    /// * It's returned whether `character_count` is less than `character_limit`.
+    /// * It's returned whether `character_count` is less or equal than `character_limit`.
     ///
     /// # Errors
     ///
@@ -32,10 +32,37 @@ impl Usage {
         &mut self,
         additional: T,
     ) -> Result<bool, <T as TryInto<u64>>::Error> {
+        let additional = additional.try_into()?;
         if let Some(character_count) = &mut self.character_count {
-            *character_count = character_count.saturating_add(T::try_into(additional)?);
+            *character_count = character_count.saturating_add(additional);
             if let Some(character_limit) = self.character_limit {
-                Ok(*character_count < character_limit)
+                Ok(*character_count <= character_limit)
+            } else {
+                Ok(false)
+            }
+        } else {
+            Ok(false)
+        }
+    }
+
+    /// Check if `character_count+additional` is within `character_limit`.
+    ///
+    /// * If `character_count` is None, false is returned.
+    /// * If `character_limit` is None, false is returned.
+    /// * It's returned whether `character_count+additional` is less or equal than `character_limit`.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` when `additional` can't be converted into a `u64`.
+    pub fn check<T: TryInto<u64>>(
+        &self,
+        additional: T,
+    ) -> Result<bool, <T as TryInto<u64>>::Error> {
+        let additional = additional.try_into()?;
+        if let Some(mut character_count) = self.character_count {
+            character_count = character_count.saturating_add(additional);
+            if let Some(character_limit) = self.character_limit {
+                Ok(character_count <= character_limit)
             } else {
                 Ok(false)
             }
